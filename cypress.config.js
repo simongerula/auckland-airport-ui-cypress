c9c721ed0464ef9619a875276a47691879988339
Simon Gerula <saimonsir@gmail.com>
2022-07-07 18:59:50 -0300

    cypress-mochawesome-reporter default

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      require('cypress-mochawesome-reporter/plugin')(on);
+    }
   },
+  screenshotOnRunFailure: true,
+  reporter:"cypress-mochawesome-reporter",
+  reporterOptions: {
+    "reportDir": "cypress/report",
+    "charts": true,
+    "embeddedScreenshots": true,
+    "saveHTML": true
+  }
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-mochawesome-reporter/register';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
@@ -22,3 +23,4 @@ import './commands'
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
 });
+
